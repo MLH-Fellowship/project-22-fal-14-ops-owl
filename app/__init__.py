eb246286e294bea884feef44cc10b7a124da76ae
Bosco_Acer <bosco.chw@gmail.com>
2022-10-10 10:41:59 -0700

    add database

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,45 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), 
+    user=os.getenv("MYSQL_USER"), 
+    password=os.getenv("MYSQL_PASSWORD"), 
+    host=os.getenv("MYSQL_HOST"), 
+    port=3306
+)
 
-@app.route('/')
-def index():
-    data = getData()
-    names = map(lambda x: x["name"].lower(), data)
-    return render_template('index.html', title="The Ops Owls", url=os.getenv("URL"), names=names)
+class TimelinePost(Model):
+  name = CharField()
+  email = CharField()
+  content = TextField()
+  created_at = DateTimeField(default=datetime.datetime.now)
+
+  class Meta:
+    database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+    }
 
 @app.route('/profile/<string:name>')
 def portfolio(name):
@@ -28,6 +57,11 @@ def portfolio(name):
 
     return render_template("profile.html", portfolio_data=portfolio_data, api_key=os.getenv("GOOGLE_MAPS_API_KEY"))
 
+@app.route('/')
+def index():
+    data = getData()
+    names = map(lambda x: x["name"].lower(), data)
+    return render_template('index.html', title="The Ops Owls", url=os.getenv("URL"), names=names)
 
 def getData():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
