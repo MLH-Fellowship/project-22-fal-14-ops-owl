1a23be58a16617a9bdfe7a0a408d292351cf0eab
bosco-chan-centos <bosco.chw@gmail.com>
2022-10-10 17:15:20 -0400

    update redeployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,8 @@
 #! /usr/bin/env bash
 
-tmux kill-session -t flask_app
 cd ~/project-22-fal-14-ops-owl
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s flask_app 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
-
-
+systemctl restart myportfolio
